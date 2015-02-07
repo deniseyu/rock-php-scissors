37fade94d80718428256335a4c1e667d670950a1
Denise <yu.denise.d@gmail.com>
2015-02-07 16:08:18 -0500

    refactor with setUp fixture

--- tests/gameTest.php
+++ tests/gameTest.php
@@ -3,50 +3,39 @@ require "./src/game.php";
 
 class GameTest extends PHPUnit_Framework_TestCase
 {
-  // protected $game;
-  // protected $rock;
-  // protected function setUp()
-  // {
-  //   $game = new Game();
-  //   $rock = new Rock();
-  //   $paper = new Paper();
-  //   $scissors = new Scissors();
-  // }
+  protected $game;
+  protected $rock;
+  protected $paper;
+  protected $scissors;
+
+  protected function setUp()
+  {
+    $this->game = new Game();
+    $this->rock = new Rock();
+    $this->paper = new Paper();
+    $this->scissors = new Scissors();
+  }
   public function testCheckType()
   {
-    $rock = new Rock();
-    $paper = new Paper();
-    $scissors = new Scissors();
-    $this->assertEquals($rock->type, "Rock");
-    $this->assertEquals($paper->type, "Paper");
-    $this->assertEquals($scissors->type, "Scissors");
+    $this->assertEquals($this->rock->type, "Rock");
+    $this->assertEquals($this->paper->type, "Paper");
+    $this->assertEquals($this->scissors->type, "Scissors");
   }
   public function testDraw()
   {
-    $game = new Game();
-    $paper = new Paper();
-    $this->assertEquals($game->evaluates($paper, $paper), "Draw");
+    $this->assertEquals($this->game->evaluates($this->paper, $this->paper), "Draw");
   }
   public function testRockBeatsScissors()
   {
-    $game = new Game();
-    $rock = new Rock();
-    $scissors = new Scissors();
-    $this->assertEquals($game->evaluates($rock, $scissors), $rock);
+    $this->assertEquals($this->game->evaluates($this->rock, $this->scissors), $this->rock);
   }
   public function testPaperBeatsRock()
   {
-    $game = new Game();
-    $rock = new Rock();
-    $paper = new Paper();
-    $this->assertEquals($game->evaluates($rock, $paper), $paper);
+    $this->assertEquals($this->game->evaluates($this->rock, $this->paper), $this->paper);
   }
   public function testScissorsBeatsPaper()
   {
-    $game = new Game();
-    $scissors = new Scissors();
-    $paper = new Paper();
-    $this->assertEquals($game->evaluates($scissors, $paper), $scissors);
+    $this->assertEquals($this->game->evaluates($this->scissors, $this->paper), $this->scissors);
   }
 }
 
